5e9261cc50d1bd616150423638927e79a63bb95a
Symrak <symrak.punk@gmail.com>
2023-04-07 20:28:07 +0300

    Update net-snmp-inventory.py
    Added draft of scanning procedure.

--- net-snmp-inventory.py
+++ net-snmp-inventory.py
@@ -6,11 +6,59 @@
 """
 Depend on external modules:
 	pip install ping3
+	pip install pysnmp
 """
 
 # Importing libraries
 from ping3 import ping
 from ipaddress import IPv4Network
+import sys, socket
+
+"""
+def udp_connection(ip, port, timeout):
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+		s.settimeout(timeout)
+		try:
+			s.sendto(b'test', (ip, port))
+			data, addr = s.recvfrom(1024)
+			return "[+] UDP Port Open: " + str(port) + str(data) + '\n'
+		except TimeoutError:
+			return "[+] UDP Port Open | Filtered: " + str(port) + '\n'
+		except:
+			return "[+] UDP Port Closed: " + str(port) + '\n'
+
+print (udp_connection("192.168.1.200", 161, 2))
+sys.exit()
+"""
+
+from pysnmp.hlapi import *
+auth = UsmUserData (
+    userName = "SNMPv3-User",
+    authKey = "authentication-pass",
+    authProtocol = usmHMACSHAAuthProtocol,
+    privKey = "priviledged-pass",
+    privProtocol = usmAesCfb128Protocol
+)
+iterator = getCmd (
+    SnmpEngine (),
+    auth,
+    UdpTransportTarget (("192.168.1.200", 161)),
+    ContextData (),
+    #ObjectType (ObjectIdentity('SNMPv2-MIB', 'sysName', 0))
+    ObjectType(ObjectIdentity(".1.3.6.1.2.1.1.1.0")),
+    ObjectType(ObjectIdentity(".1.3.6.1.4.1.12356.100.1.1.1.0")),
+    lookupMib = False
+)
+errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+if errorIndication:
+	print(errorIndication)
+elif errorStatus:
+	print('%s at %s' % (errorStatus.prettyPrint(),
+	errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+else:
+	for varBind in varBinds:
+		print(' = '.join([x.prettyPrint() for x in varBind]))
+sys.exit()
 
 # Reading input
 scanAddress = IPv4Network("192.168.1.0/29")
