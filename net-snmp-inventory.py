576bfe6c28514898756da472904a8db4c9300917
Symrak <symrak.punk@gmail.com>
2023-06-02 23:50:09 +0300

    Update net-snmp-inventory.py
    Processed values of general OIDs. Tuned console output.

--- net-snmp-inventory.py
+++ net-snmp-inventory.py
@@ -24,6 +24,8 @@ scanAddress = IPv4Network("192.168.1.192/28")
 # snmpHost = "192.168.1.200"
 snmpPort = 161
 snmpIterMaxCount = 256
+snmpRetriesCount = 0
+snmpTimeout = 2.0
 snmpUsername = "SNMPv3-User"
 snmpAuthKey = "authentication-pass"
 snmpAuthProtocol = usmHMACSHAAuthProtocol
@@ -50,11 +52,11 @@ print (udp_connection("192.168.1.200", 161, 2))
 sys.exit()
 """
 
-def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtocol=usmHMACSHAAuthProtocol, snmpPrivProtocol=usmAesCfb128Protocol, snmpPort=161, snmpIterMaxCount=256):
+def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtocol=usmHMACSHAAuthProtocol, snmpPrivProtocol=usmAesCfb128Protocol, snmpPort=161, snmpIterMaxCount=256, snmpRetriesCount=0, snmpTimeout=2.0):
 	# Function variables
-	snmpDataDict = {snmpHost : {"Sysname" : "N/A", "Manufacturer" : "N/A", "Model" : "N/A", "FW" : "N/A",
-																"S/N" : "N/A", "Location" : "N/A", "Description" : "N/A", "Contact" : "N/A",
-																"MAC Address" : "N/A", "IP Addresses" : [], "PING" : True, "SNMP" : True}}
+	snmpDataDict = {snmpHost : {"Sysname" : None, "Manufacturer" : None, "Model" : None, "FW" : None,
+																"S/N" : None, "Location" : None, "Description" : None, "Contact" : None,
+																"MAC Address" : None, "IP Addresses" : [], "PING" : True, "SNMP" : False}}
 	# Authentication data
 	snmpAuth = UsmUserData (
 		userName = snmpUsername,
@@ -68,7 +70,7 @@ def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtoco
 	snmpRequest = getCmd (
 		SnmpEngine (),
 		snmpAuth,
-		UdpTransportTarget ((snmpHost, snmpPort)),
+		UdpTransportTarget ((snmpHost, snmpPort), retries=snmpRetriesCount, timeout=snmpTimeout),
 		ContextData (),
 		# System Name @ sysName!@#.iso.org.dod.internet.mgmt.mib-2.system.sysName (.1.3.6.1.2.1.1.5.0)
 		ObjectType(ObjectIdentity("SNMPv2-MIB", "sysName", 0)),
@@ -97,22 +99,39 @@ def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtoco
 	)
 	errorIndication, errorStatus, errorIndex, varBinds = next(snmpRequest)
 	if errorIndication:
-		print(errorIndication)
+		print("\t[WARN!] IP %s [SNMP - General Info] - %s" % (snmpHost, errorIndication))
 	elif errorStatus:
-		print("%s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
+		print("\t[ERROR!] %s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
 	else:
+		# Array for storing SNMP values
+		varBindValues = []
+		# Extracting SNMP OIDs and their Values
 		for varBind in varBinds:
-			print(" = ".join([x.prettyPrint() for x in varBind]))
+			### DEBUG: Pretty output of SNMP library
+			# print(" = ".join([x.prettyPrint() for x in varBind]))
 			name, value = varBind
-			print("\tOID = %s" % name)
-			print("\tValue = %s" % value)
-
+			varBindValues.append(str(value))
+			### DEBUG: OID and value output
+			# print("\tOID = %s" % name)
+			# print("\tValue = %s" % value)
+		# Filling-ip dictionary with array values
+		valuesCount = len(varBindValues)
+		i = 0
+		for key in snmpDataDict[snmpHost]:
+			if ((varBindValues[i]) != None and len(varBindValues[i]) > 0):
+				snmpDataDict[snmpHost][key] = varBindValues[i]
+			if i < valuesCount-1:
+				i += 1
+			else:
+				break
+		# Flipping SNMP state flag
+		snmpDataDict[snmpHost]["SNMP"] = True
 	# SNMP GET-NEXT requests payload & processing
 	# MAC address collecting (only interface #1)
 	snmpRequest = nextCmd (
 		SnmpEngine (),
 		snmpAuth,
-		UdpTransportTarget ((snmpHost, snmpPort)),
+		UdpTransportTarget ((snmpHost, snmpPort), retries=snmpRetriesCount, timeout=snmpTimeout),
 		ContextData (),
 		# MAC Address @ ifPhysAddress!@#.iso.org.dod.internet.mgmt.mib-2.interfaces.ifTable.ifEntry.ifPhysAddress
 		ObjectType(ObjectIdentity(".1.3.6.1.2.1.2.2.1.6")),
@@ -121,23 +140,26 @@ def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtoco
 	)
 	errorIndication, errorStatus, errorIndex, varBinds = next(snmpRequest)
 	if errorIndication:
-		print(errorIndication)
+		print("\t[WARN!] IP %s [SNMP - MAC Addresses] - %s" % (snmpHost, errorIndication))
 	elif errorStatus:
-		print("%s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
+		print("\t[ERROR!] %s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
 	else:
+		# Extracting SNMP OIDs and their Values
 		for varBind in varBinds:
 			### DEBUG: Pretty output of SNMP library
 			# print(" = ".join([x.prettyPrint() for x in varBind]))
 			name, value = varBind
-			snmpDataDict[snmpHost]["MAC Address"] = str(macaddress.MAC(bytes(value))).replace("-", ":")
+			snmpDataDict[snmpHost]["MAC Address"] = str(macaddress.MAC(bytes(value))).replace("-", ":").lower()
 			### DEBUG: OID and MAC value output
 			# print("\tOID = %s" % name)
 			# print("\tValue = %s" % str(macaddress.MAC(bytes(value))).replace("-", ":"))
+		# Flipping SNMP state flag
+		snmpDataDict[snmpHost]["SNMP"] = True
 	# IP address collecting (all available)
 	snmpRequest = nextCmd (
 		SnmpEngine (),
 		snmpAuth,
-		UdpTransportTarget ((snmpHost, snmpPort)),
+		UdpTransportTarget ((snmpHost, snmpPort), retries=snmpRetriesCount, timeout=snmpTimeout),
 		ContextData (),
 		# IP Addresses @ ipAdEntAddr!@#.iso.org.dod.internet.mgmt.mib-2.ip.ipAddrTable.ipAddrEntry.ipAdEntAddr
 		ObjectType(ObjectIdentity("IP-MIB", "ipAdEntAddr")),
@@ -149,10 +171,11 @@ def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtoco
 		try:
 			errorIndication, errorStatus, errorIndex, varBinds = next(snmpRequest)
 			if errorIndication:
-				print(errorIndication)
+				print("\t[WARN!] IP %s [SNMP - IP Addresses] - %s" % (snmpHost, errorIndication))
 			elif errorStatus:
-				print("%s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
+				print("\t[ERROR!] %s at %s" % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or "?"))
 			else:
+				# Extracting SNMP OIDs and their Values
 				for varBind in varBinds:
 					### DEBUG: Pretty output of SNMP library
 					# print(" = ".join([x.prettyPrint() for x in varBind]))
@@ -164,25 +187,35 @@ def snmp_audit(snmpHost, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtoco
 			snmpIterCount += 1
 		except StopIteration:
 			break
+	# Filling-ip IP address with None if there are no any addresses
+	if len(snmpDataDict[snmpHost]["IP Addresses"]) == 0:
+		snmpDataDict[snmpHost]["IP Addresses"] = None
+	else:
+		# Flipping SNMP state flag
+		snmpDataDict[snmpHost]["SNMP"] = True
 	return snmpDataDict
 
 # Calculating the network
 netAddress = scanAddress.network_address
 netBroadcastAddress = scanAddress.broadcast_address
 netDescription = str(netAddress) + "/" + str(scanAddress.prefixlen)
-print("\nThe given network is %s (%s), consists of %s hosts." % (netDescription, scanAddress.netmask, scanAddress.num_addresses-2))
+netAddressesCount = scanAddress.num_addresses-2
+print("\nThe given network is %s (%s), consists of %s hosts.\n" % (netDescription, scanAddress.netmask, netAddressesCount))
 
 # Generating host dictionary
 netScanDict = {netDescription : {}}
 for hostAddress in scanAddress:
 	if ((hostAddress != netAddress) and (hostAddress != netBroadcastAddress)):
-		netScanDict[netDescription].update({str(hostAddress) : {"Sysname" : "N/A", "Manufacturer" : "N/A", "Model" : "N/A", "FW" : "N/A",
-																"S/N" : "N/A", "Location" : "N/A", "Description" : "N/A", "Contact" : "N/A",
-																"MAC Address" : "N/A", "IP Addresses" : [], "PING" : False, "SNMP" : False}})
+		netScanDict[netDescription].update({str(hostAddress) : {"Sysname" : None, "Manufacturer" : None, "Model" : None, "FW" : None,
+																"S/N" : None, "Location" : None, "Description" : None, "Contact" : None,
+																"MAC Address" : None, "IP Addresses" : None, "PING" : False, "SNMP" : False}})
 
 # Performing host discovery & SNMP audit
+currentAddressNumber = 1
+print("Scanning hosts (ICMP PING discovery + SNMP requests):")
 for hostAddress in netScanDict[netDescription]:
 	# Performing ICMP PING host discovery
+	print("\tProgress: IP %s [PING] - %s of %s (%.2f%%)" % (hostAddress, currentAddressNumber, netAddressesCount, currentAddressNumber/netAddressesCount*100), end="\r")
 	checkResult = ping(hostAddress)
 	### DEBUG: PING value output in miliseconds
 	# print(round(checkResult, 2))
@@ -190,12 +223,16 @@ for hostAddress in netScanDict[netDescription]:
 	netScanDict[netDescription][hostAddress]["PING"] = hostIsActive
 	# Performing SNMP host audit
 	if hostIsActive:
-		netScanDict[netDescription].update(snmp_audit(hostAddress, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtocol, snmpPrivProtocol, snmpPort, snmpIterMaxCount))
+		print("\tProgress: IP %s [SNMP] - %s of %s (%.2f%%)" % (hostAddress, currentAddressNumber, netAddressesCount, currentAddressNumber/netAddressesCount*100), end="\r")
+		netScanDict[netDescription].update(snmp_audit(hostAddress, snmpUsername, snmpAuthKey, snmpPrivKey, snmpAuthProtocol, snmpPrivProtocol, snmpPort, snmpIterMaxCount, snmpRetriesCount, snmpTimeout))
+	# Incrementing address number
+	currentAddressNumber += 1
 
 # Printing out the results
-print("\nThe scan results for network %s are:" % (netDescription))
+print("\n\nThe scan results for network %s are:" % (netDescription))
 for hostAddress in netScanDict[netDescription]:
 	resultString = "\t " + hostAddress + ": "
 	for element in netScanDict[netDescription][hostAddress]:
 		resultString = resultString + element + " = " + str(netScanDict[netDescription][hostAddress][element]) + "; "
 	print(resultString)
+print("\n")
